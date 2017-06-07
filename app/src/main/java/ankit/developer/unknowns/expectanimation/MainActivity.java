6fd1942a0b3fce500705eb6b4621d9ef75e58c75
Ankit sharma <unknownsdeveloper@gmail.com>
2017-06-07 17:42:58 +0530

    Initial commit

---
+++ app/src/main/java/ankit/developer/unknowns/expectanimation/MainActivity.java
@@ -0,0 +1,56 @@
+package ankit.developer.unknowns.expectanimation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.florent37.expectanim.ExpectAnim;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.github.florent37.expectanim.core.Expectations.height;
+import static com.github.florent37.expectanim.core.Expectations.invisible;
+import static com.github.florent37.expectanim.core.Expectations.scale;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.sample)
+    public void onSampleClicked() {
+        startActivity(new Intent(this, SampleActivity.class));
+    }
+
+    @OnClick(R.id.scroll)
+    public void onScrollClicked() {
+        startActivity(new Intent(this, ScrollActivity.class));
+    }
+
+    @OnClick(R.id.rotation)
+    public void onRotationClicked() {
+        startActivity(new Intent(this, RotationActivity.class));
+    }
+
+    @OnClick(R.id.flip)
+    public void onFlipClicked() {
+        startActivity(new Intent(this, FlipActivity.class));
+    }
+
+    @OnClick(R.id.setnow)
+    public void onSetNowClicked() {
+        startActivity(new Intent(this, SetNowActivity.class));
+    }
+
+    @OnClick(R.id.visible)
+    public void onVisibleClicked() {
+        startActivity(new Intent(this, AlphaActivity.class));
+    }
+
+}
